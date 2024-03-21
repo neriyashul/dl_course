cccdb09cc33cc6ce90a8f0e08295882ab28a319e
neriya.shulman <neriyashul@gmail.com>
2024-03-21 02:57:24 +0200

    add early stopping

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from os import path
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Normalization
 from keras.preprocessing import image_dataset_from_directory
+from keras.callbacks import EarlyStopping
+from keras.metrics import Recall, Precision, F1Score
 from matplotlib import pyplot as plt
 import numpy as np
 import tensorflow as tf
@@ -12,7 +14,6 @@ import tensorflow as tf
 
 # TODO: delete it
 def show_img(image):
-    image_np = image.numpy()[:, :, 0]  # Convert to numpy array
     # Display the image with matplotlib
     plt.imshow(image.numpy(), cmap='gray')
     plt.axis('off')  # Optionally, turn off the axis.
@@ -48,9 +49,10 @@ for images, labels in train_ds.take(1):
 base_dir = "/Users/neriya.shulman/content/chest_xray"
 
         
-batch_size = 32
-img_height = 64
-img_width = 64
+batch_size = 256
+img_height = 128
+img_width = 128
+max_epochs = 100
 
 print("Image size: ", img_height, "x", img_width)
 print("Batch size: ", batch_size)
@@ -59,7 +61,7 @@ train_ds = image_dataset_from_directory(
     directory=path.join(base_dir, "train/"),
     # validation_split=0.2,
     # subset="training",
-    # seed=123,
+    seed=123,
     label_mode='binary',  # 'categorical' for multiclasses
     color_mode='grayscale',
     image_size=(img_height, img_width),
@@ -70,7 +72,7 @@ validation_ds = image_dataset_from_directory(
     directory=path.join(base_dir, "val/"),
     # validation_split=0.2,
     # subset="validation",
-    # seed=123,
+    seed=123,
     label_mode='binary',  # or 'categorical'
     color_mode='grayscale',
     image_size=(img_height, img_width),
@@ -93,20 +95,19 @@ Normalization layer
 '''
 
 # Create the normalization layer
-normalization_layer = Normalization(axis=-1)
+normalization_layer = Normalization()
 
 # Adapt the normalization layer with your dataset
 # Use the map function to extract just the image data if your dataset also contains labels
-normalization_layer.adapt(train_ds.map(lambda x, y: x))
+normalization_layer.adapt(train_ds.map(lambda x, _: x))
 # ---------------------------------------------------------------------
+
 
 # Build the CNN Model
 
 model = Sequential()
 
 model.add(normalization_layer)
-
-model.fit(train_ds, epochs=10, validation_data=validation_ds)
 
 
 # Convolution layer
@@ -126,10 +127,31 @@ model.add(Dense(units=128, activation='relu'))
 model.add(Dense(units=1, activation='sigmoid'))  # Use 'softmax' for more than 2 classes
 
 # Compile the model
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', Recall(), Precision(), F1Score(threshold=0.5)])
+
+# add early stopping
+early_stopping = EarlyStopping(
+    monitor='val_loss', patience=3, verbose=1, mode='min'
+)
+
+model.fit(train_ds, epochs=max_epochs, validation_data=validation_ds, callbacks=[early_stopping])
+
+results = model.evaluate(test_ds)
+
+print("---------------------------------------------------")
+print("Test results: ")
+print(f"Loss: {results[0]}")
+print(f"Accuracy: {results[1]*100:.2f}%")
+print(f"Recall: {results[2]*100:.2f}%")
+print(f"Precision: {results[3]*100:.2f}%")
+print(f"F1 score: {results[4]*100:.2f}%")
 
 
-model.fit(train_ds, epochs=10, validation_data=validation_ds)
 
-loss, accuracy = model.evaluate(test_ds)
-print(f"Test accuracy: {accuracy*100:.2f}%")
+# save the model
+model.save('my_model.keras')
+
+# load the model
+# model = tf.keras.models.load_model('cnn_model.h5')
+
+
