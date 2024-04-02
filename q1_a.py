b6f26814bb129ad17394a041a657433dc27d8530
neriya.shulman <neriyashul@gmail.com>
2024-04-02 08:33:28 +0300

    fix q1_a

--- q1_a.py
+++ q1_a.py
@@ -1,198 +1,142 @@
-from math import pi
-from os import path
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Normalization, RandomRotation, Dropout, RandomZoom, Cropping2D, BatchNormalization, RandomContrast
-from keras.callbacks import EarlyStopping
-from keras.metrics import Recall, Precision, F1Score
-from matplotlib import pyplot as plt
-import numpy as np
-import tensorflow as tf
-from utils import load_data
-from keras.optimizers import Adam
-import numpy as np
+import os
+import keras
 import random
+import numpy as np
 import tensorflow as tf
+from os import path
+from keras import layers
+from keras.optimizers import Adam
+from keras.layers import Dense, RandomBrightness, RandomRotation, RandomZoom, RandomContrast
+from keras.callbacks import EarlyStopping
+from utils import load_data, add_samples_to_validation, shuffle_dataset
 
 # Set seeds
 np.random.seed(42)
 random.seed(42)
 tf.random.set_seed(42)
 
-
-
-if tf.config.list_physical_devices('GPU'):
-    print("Using GPU :)")
-else:
-    print("No GPU :/")
-
-
-# ---------------------------------------------------------------------
-
-
-# TODO: delete it
-def show_img(image):
-    # Display the image with matplotlib
-    plt.imshow(image.numpy(), cmap='gray')
-    plt.axis('off')  # Optionally, turn off the axis.
-    plt.show()
-'''
-# how to use:
-for images, labels in train_set:
-    show_img(images[0])
-    break
-'''    
-
-    
-# TODO: delete it
-def show_histogram(image):
-    # Display the histogram
-    
-    # show the values on the x-axis and the frequency on the y-axis
-    plt.hist(np.array(image).ravel(), bins=256)
-    plt.show()
-'''
-# how to use:
-for images, labels in train_set.take(1):
-    normalized_image = normalization_layer(images[0][tf.newaxis, ...])
-    show_histogram(normalized_image)
-    break
-'''
-
-
 base_dir = "/Users/neriya.shulman/content/chest_xray"
 
-# TODO: change it to 64x64
 
+img_height = 250
+img_width = 250
+max_epochs = 50
 batch_size = 64
-img_height = 150
-img_width = 150
-max_epochs = 100
+learning_rate=0.001
 
 print("Image size: ", img_height, "x", img_width)
 print("Batch size: ", batch_size)
 
 
+def balance_data(normal_data, pneum_data):
+    multiply_by = round(len(pneum_data) / len(normal_data))
+    return np.concatenate([normal_data for _ in range(multiply_by)], axis=0)
+
+
+def get_balanced_data(base_dir):
+    print('get_balanced_data')
+    normal_train_set, normal_train_labels = load_data(path.join(base_dir, "NORMAL"), img_height, img_width, all_labels)
+    pneum_train_set, pneum_train_labels = load_data(path.join(base_dir, "PNEUMONIA"), img_height, img_width, all_labels)
+    normal_train_set = balance_data(normal_train_set, pneum_train_set)
+    normal_train_labels = balance_data(normal_train_labels, pneum_train_labels)
+    
+    train_set = np.concatenate([normal_train_set, pneum_train_set], axis=0)
+    train_labels = np.concatenate([normal_train_labels, pneum_train_labels], axis=0)
+    
+    train_set, train_labels = shuffle_dataset(train_set, train_labels)
+    return train_set, train_labels
+
+
 all_labels = ['NORMAL', 'PNEUMONIA']
-train_path = path.join(base_dir, "train/")
-train_set, train_labels = load_data(train_path, img_height, img_width, all_labels, shuffle=True)
+train_set, train_labels = get_balanced_data(os.path.join(base_dir, "train"))
 
 val_path = path.join(base_dir, "val/")
 validation_set, validation_labels = load_data(val_path, img_height, img_width, all_labels)
+if len(validation_set) < 20:
+    add_samples_to_validation(base_dir)
 
 test_path = path.join(base_dir, "test/")
 test_set, test_labels = load_data(test_path, img_height, img_width, all_labels)
 
 print('Number of classes: ', len(all_labels))
-print('Number of training samples: ', len(train_set//batch_size))
+print('Number of training samples: ', len(train_set)//batch_size)
 print('Number of total samples: ', len(train_set))
 
-# ---------------------------------------------------------------------
-'''
-Normalization layer
-'''
-# TODO check if needed or delete it: 
-norm_layer = Normalization(axis=None)
-norm_layer.adapt(train_set)
-# ---------------------------------------------------------------------
 
-crop_percent = 3
-height_crop = int(img_height * crop_percent / 100)
-width_crop = int(img_width * crop_percent / 100)
-print("Crop percent: ", crop_percent)
+model = keras.Sequential(
+    [
+        keras.Input(shape=(img_height, img_width, 1)),
+        RandomRotation(0.35),
+        RandomZoom(0.1),
+        RandomBrightness(0.15, value_range=(0,1)),
+        RandomContrast(0.1),
+        
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        Dense(units=128, activation='relu'),
+        Dense(units=64, activation='relu'),
+        layers.Dropout(0.5),
+        Dense(units=32, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(units=1, activation='sigmoid'),
+        
+        # # option 2
+        # Conv2D(32, (3, 3), activation='relu'),
+        # MaxPooling2D(pool_size=(2, 2)),
+        # Conv2D(64, (3, 3), activation='relu'),
+        # MaxPooling2D(pool_size=(2, 2)),
+        # Conv2D(128, (3, 3), activation='relu'),
+        # MaxPooling2D(pool_size=(2, 2)),
+        # Dropout(0.5),
+        # Conv2D(256, (3, 3), activation='relu'),
+        # Flatten(name='flatten'),
+        # Dense(units=516, activation='relu'),
+        # Dense(units=256, activation='relu'),
+        # Dense(units=128, activation='relu'),
+        # Dropout(0.5),
+        # Dense(units=1, activation='sigmoid')
+        
+        # option 3
+        # Conv2D(32, (3, 3), activation='relu', padding='same'),
+        # MaxPooling2D(pool_size=(2, 2)),
+        # Conv2D(64, (3, 3), activation='relu', padding='same'),
+        # MaxPooling2D(pool_size=(2, 2)),
+        # Conv2D(128, (3, 3), activation='relu', padding='same'),
+        # MaxPooling2D(pool_size=(2, 2)),
+        # Dropout(0.25),
+        # Conv2D(256, (3, 3), activation='relu', padding='same'),
+        # MaxPooling2D(pool_size=(2, 2)),
+        # Conv2D(516, (3, 3), activation='relu', padding='same'),
+        # Flatten(name='flatten'),
+        # Dense(units=1024, activation='relu'),
+        # Dense(units=526, activation='relu'),
+        # Dropout(0.25),
+        # Dense(units=256, activation='relu'),
+        # Dense(units=128, activation='relu'),
+        # Dropout(0.5),
+        # Dense(units=1, activation='sigmoid')
+    ]
+)
 
-# Build the CNN Model
-
-model = Sequential()
-model.add(norm_layer) # TODO: remove this
-model.add(Cropping2D(cropping=(height_crop, width_crop), input_shape=(img_height, img_width, 1))),
-
-# add rabdom rotation of 10 degrees clockwise or counterclockwise
-degrees = 20
-print("Rotation: ", degrees)
-model.add(RandomRotation(degrees*pi/180))
-# model.add(BatchNormalization())
-
-
-# option 1 (89.9%):
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Flatten(name='flatten'))
-model.add(Dense(units=128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(units=1, activation='sigmoid'))
-
-# # option 2 (89%):
-# img_height = 200
-# img_width = 200
-# model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Dropout(0.5))
-# model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-# model.add(Flatten(name='flatten'))
-# model.add(Dense(units=516, activation='relu'))
-# model.add(Dense(units=256, activation='relu'))
-# model.add(Dense(units=128, activation='relu'))
-# model.add(Dropout(0.5))
-# model.add(Dense(units=1, activation='sigmoid'))
-
-
-
-# # option 3 (90.54%):
-# model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Dropout(0.25))
-# model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Conv2D(516, (3, 3), activation='relu', padding='same'))
-# model.add(Flatten(name='flatten'))
-# model.add(Dense(units=1024, activation='relu'))
-# model.add(Dense(units=526, activation='relu'))
-# model.add(Dropout(0.25))
-# model.add(Dense(units=256, activation='relu'))
-# model.add(Dense(units=128, activation='relu'))
-# model.add(Dropout(0.5))
-# model.add(Dense(units=1, activation='sigmoid'))
+model.compile(loss="binary_crossentropy", optimizer=Adam(learning_rate), metrics=["accuracy", "recall", "precision"])
 
 
-# learning_rate=0.01 # TODO too large learning rate
+early_stopping = EarlyStopping(monitor='val_loss', patience=5, verbose=1)
 
+history = model.fit(train_set, train_labels, epochs=max_epochs, batch_size=batch_size, 
+        validation_data=(validation_set, validation_labels), callbacks=[early_stopping])
 
-# TODO: delete it 
-# gamma = 2
-# loss = BinaryFocalCrossentropy(alpha=0.25, gamma=gamma)
-
-# Compile the model
-model.compile(optimizer="adam", loss='binary_crossentropy', metrics=['accuracy', Recall(), Precision()])
-
-# add early stopping
-# add early stopping
-early_stopping = EarlyStopping(monitor='val_loss', patience=3, verbose=1)
-
-
-model.fit(train_set, train_labels, epochs=max_epochs, batch_size=batch_size, 
-          validation_data=(validation_set, validation_labels), callbacks=[early_stopping])
 
 print(model.summary())
-results = model.evaluate(test_set, test_labels, batch_size=batch_size)
 
 
-
-print("---------------------------------------------------")
-print("Test results: ")
-print(f"Loss: {results[0]}")
-print(f"Accuracy: {results[1]*100:.2f}%")
-print(f"Recall: {results[2]*100:.2f}%")
-print(f"Precision: {results[3]*100:.2f}%")
+score = model.evaluate(test_set, test_labels, batch_size=batch_size)
+# results = model.evaluate(test_set, test_labels, batch_size=batch_size)
+print("Test loss:", score[0])
+print("Test accuracy:", score[1])
 
 
 
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 
 import os
+import random
+import shutil
 from matplotlib import pyplot as plt
 import numpy as np
 import tensorflow as tf
@@ -7,25 +9,29 @@ from keras.preprocessing.image import load_img
 from keras.utils import to_categorical
 
 
-def get_label(dir_name, file_name, file_labels=None):
-    if file_labels is None:
-        return dir_name
+
+def get_label(dir_name, file_name, file_labels=[]):
     for label in file_labels:
         if label in file_name:
             return label
+    return dir_name
 
-def load_data_from_path(base_dir, img_height, img_width, file_labels=None):
+def load_data_from_path(base_dir, img_height, img_width, file_labels=[]):
     dataset = []
     labels = []
     img_size = (img_height, img_width)
-    for dir in os.scandir(base_dir):
-        if not dir.is_dir():
+    dir_names = os.path.basename(base_dir)
+    for file in os.scandir(base_dir):
+        if file.name == '.DS_Store':
             continue
-        for file in os.scandir(dir.path):
-            # load and rescale to be between 0 and 1
+        if file.is_dir():
+            dir_images, dir_labels = load_data_from_path(file.path, img_height, img_width, file_labels)
+            dataset.extend(dir_images)
+            labels.extend(dir_labels)
+        else:
             img = load_img(file.path, target_size=img_size, color_mode='grayscale')
             dataset.append(img)
-            labels.append(get_label(dir.name, file.name, file_labels))
+            labels.append(get_label(dir_names, file.name, file_labels))
     dataset = np.array(dataset, dtype='float32')
     labels = np.array(labels)
     return dataset, labels
@@ -35,15 +41,13 @@ def shuffle_dataset(dataset, labels):
     np.random.shuffle(indices)
     return dataset[indices], labels[indices]
 
-def load_data(path, img_height, img_width, all_labels, shuffle=False, file_labels=None):
+def load_data(path, img_height, img_width, all_labels, shuffle=False, file_labels=[]):
     dataset, labels = load_data_from_path(path, img_height, img_width, file_labels)
     # TODO: check if needed or delete it: 
     dataset /= 255 # rescale to be between 0 and 1
     labels = np.array([all_labels.index(label) for label in labels]) # one-hot encoding
     if len(all_labels) > 2:
-        labels = to_categorical(labels)
-    if shuffle:
-        dataset, labels = shuffle_dataset(dataset, labels)
+        labels = to_categorical(labels, len(all_labels))
     dataset = np.expand_dims(dataset, -1) # image should be (height, width, channels)
     labels = np.expand_dims(labels, -1)
     return dataset, labels
@@ -53,7 +57,7 @@ def load_data(path, img_height, img_width, all_labels, shuffle=False, file_label
 # TODO: delete it
 def show_img(image):
     # Display the image with matplotlib
-    plt.imshow(image.numpy(), cmap='gray')
+    plt.imshow(np.array(image), cmap='gray')
     plt.axis('off')  # Optionally, turn off the axis.
     plt.show()
 '''
@@ -78,3 +82,26 @@ for images, labels in train_ds.take(1):
     show_histogram(normalized_image)
     break
 '''
+
+def move_files(source_dir, dest_dir, num_to_move, filter_fn=lambda x: True):
+    filenames = [f.name for f in os.scandir(source_dir) if f.is_file() and filter_fn(f.name)]
+    np.random.shuffle(filenames)
+    for filename in filenames[:num_to_move]:
+        src_path = os.path.join(source_dir, filename)
+        dest_path = os.path.join(dest_dir, filename)
+        shutil.move(src_path, dest_path)
+
+def add_samples_to_validation(base_dir, num_to_move=100):
+    source_folder = os.path.join(base_dir, 'train')
+    destination_folder = os.path.join(base_dir, 'val')
+
+    for dir_name in ['NORMAL', 'PNEUMONIA']:
+        source_dir = os.path.join(source_folder, dir_name)
+        dest_dir = os.path.join(destination_folder, dir_name)
+        os.makedirs(dest_dir, exist_ok=True)
+
+        if dir_name == 'NORMAL':
+            move_files(source_dir, dest_dir, num_to_move * 2)
+        else:
+            move_files(source_dir, dest_dir, num_to_move, lambda x: 'bacteria' in x)
+            move_files(source_dir, dest_dir, num_to_move, lambda x: 'virus' in x)
