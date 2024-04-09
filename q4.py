afae18a592365f00937b144c34555136e0b1cfee
Raviv <noname@noaddress.com>
2024-04-09 13:25:52 +0300

    Q4. Visualization of the model layers.

---
+++ q4.py
@@ -0,0 +1,131 @@
+
+from keras.models import Model, load_model
+from keras.preprocessing.image import load_img, img_to_array
+import matplotlib.pyplot as plt
+import numpy as np
+from os import getcwd
+from os.path import join
+
+
+#base_dir = "/Users/neriya.shulman/content/chest_xray"
+base_dir = join(getcwd(), "chest_xray")
+
+
+def load_q1_a_model():
+    return load_model('my_model.keras')
+
+
+def print_models_layers(model) -> None:
+    for idx, layer in enumerate(model.layers):
+        print(idx, layer)
+
+
+def print_specific_layer_in_model_with_relevant_name(model, layer_base_name="conv2d") -> None:
+    for idx, layer in enumerate(model.layers):
+        if layer_base_name in model.layers[idx].name:
+            print("layer: ", idx, model.layers[idx].name)
+
+
+def print_all_layers_with_weights(model) -> None:
+    for idx, layer in enumerate(model.layers):
+        if model.layers[idx].get_weights():
+            print("layer: ", idx, model.layers[idx].name)
+            print("filter weights: ", model.layers[idx].get_weights()[0].shape)
+            print("output weights: ", model.layers[idx].get_weights()[1].shape)
+            print()
+
+
+def get_layer_filters(model, layer_name: str, include_prints) -> list:
+    lst = []
+    for idx, layer in enumerate(model.layers):
+        if layer_name == model.layers[idx].name:
+            if include_prints:
+                print("layer: ", idx, model.layers[idx].name)
+                print("filter weights: ", model.layers[idx].get_weights()[0].shape)
+                print("output weights: ", model.layers[idx].get_weights()[1].shape)
+                print()
+            for idx2 in range(model.layers[idx].get_weights()[0].shape[3]):
+                x = model.layers[idx].get_weights()[0][:, :, :, idx2]
+                lst.append(x)
+
+            break
+    return lst
+
+
+def display_first_layer_filters(filters_list: list) -> None:
+    lst_width = 8
+    lst_height = int(len(filters_list) / lst_width)
+    fig, ax = plt.subplots(lst_height, lst_width, figsize=(12, 6))
+    fig.suptitle(f"{len(filters_list)}x{'x'.join((str(b) for b in filters_list[0].shape))} Filters Displays")
+    for i in range(lst_height):
+        for j in range(lst_width):
+            ax[i, j].imshow(filters_list[i * lst_width + j], cmap='gray')
+            ax[i, j].axis('off')
+    plt.show()
+
+
+def display_seconds_layer_filters(filters_list: list) -> None:
+    lst = []
+    for pic in filters_list:
+        lst.append(np.concatenate(pic, axis=1))
+
+    fig, ax = plt.subplots(len(lst), 1, figsize=(18, 36))
+    fig.suptitle(f"{len(lst)}x{'x'.join((str(b) for b in filters_list[0].shape))} Filters Displays")
+    for i, pic in enumerate(lst):
+        ax[i].imshow(pic, cmap='gray')
+        ax[i].axis('off')
+    plt.show()
+
+
+def display_layer_outputs_of_image(model, img_path: str, layer_name: str = 'conv2d_1') -> None:
+    width_size = 8
+    height_size = 8
+    layer_outputs = [layer.output for layer in model.layers if layer.name == layer_name]
+    activation_model = Model(inputs=model.inputs[0], outputs=layer_outputs)
+
+    img = load_img(img_path, target_size=(250, 250)).convert('L')
+    img_tensor = img_to_array(img)
+    img_tensor = np.expand_dims(img_tensor, axis=0)
+    img_tensor /= 255.
+
+    activations = activation_model.predict(img_tensor)
+    first_layer_activation = activations[0]
+
+    fig, ax = plt.subplots(height_size, width_size, figsize=(24, 24))
+    fig.suptitle(f"Display output of layer {layer_name}")
+    for height_idx in range(height_size):
+        for width_idx in range(width_size):
+            ax[height_idx, width_idx].imshow(first_layer_activation[:, :, height_idx*8+width_idx]*255., cmap='gray')
+            ax[height_idx, width_idx].axis('off')
+    plt.show()
+
+
+def display_layer_outputs_of_normal_img(model):
+    base_tested_dir = join(base_dir, join("test", "NORMAL"))
+    tested_image_name = "IM-0001-0001.jpeg"
+    img_path = join(base_tested_dir, tested_image_name)
+    display_layer_outputs_of_image(model, img_path)
+
+
+def display_layer_outputs_of_pneumonia_img(model):
+    base_tested_dir = join(base_dir, join("test", "PNEUMONIA"))
+    tested_image_name = "person1_virus_6.jpeg"
+    img_path = join(base_tested_dir, tested_image_name)
+    display_layer_outputs_of_image(model, img_path)
+
+
+def main(include_prints: bool = False):
+    q1_a_model = load_q1_a_model()
+    if include_prints:
+        print_models_layers(q1_a_model)
+        print_specific_layer_in_model_with_relevant_name(q1_a_model, "conv2d")
+        print_specific_layer_in_model_with_relevant_name(q1_a_model, "dense")
+        print_all_layers_with_weights(q1_a_model)
+    display_first_layer_filters(get_layer_filters(q1_a_model, "conv2d", include_prints))
+    display_seconds_layer_filters(get_layer_filters(q1_a_model, "conv2d_1", include_prints))
+    display_layer_outputs_of_normal_img(q1_a_model)
+    display_layer_outputs_of_pneumonia_img(q1_a_model)
+
+
+if __name__ == "__main__":
+    main()
